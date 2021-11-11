0475b80547c9df8a5f01198fb2c343f32e887dbc
Alison Clare <alisonclare93@gmail.com>
2021-11-11 16:24:49 +0000

    created new list function

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,6 +1,7 @@
 class ListsController < ApplicationController
   def index
     @lists = List.all
+    @list = List.new
   end
 
   def show
@@ -9,10 +10,18 @@ class ListsController < ApplicationController
   end
 
   def new
-
+    @list = List.new
   end
 
   def create
+    @list = List.create(list_params)
 
+    redirect_to lists_path
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
   end
 end
