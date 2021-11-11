363659c1db42a17c72dddf6932350e92663d628a
Alison Clare <alisonclare93@gmail.com>
2021-11-11 14:19:17 +0000

    seeded database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+# instead of doing db:drop - this will clear data before creating again
+Movie.destroy_all
+
+url = 'http://tmdb.lewagon.com/movie/top_rated'
+movie_serialized = URI.open(url).read
+movies = JSON.parse(movie_serialized)['results']
+poster_url = "https://image.tmdb.org/t/p/w500"
+
+movies.each do |movie|
+  puts "creating #{movie['title']}"
+  Movie.create(
+    title: movie['title'],
+    overview: movie['overview'],
+    poster_url: poster_url + movie['poster_path'],
+    rating: movie['vote_average'],
+
+  )
+end
